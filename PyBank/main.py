16a5be63f46240b2091c17c29c186b9ff2b934a0
Robert Franklin <robertlsfranklin@gmail.com>
2022-11-19 17:25:12 +0800

    update2

---
+++ PyBank/main.py
@@ -0,0 +1,33 @@
+## PyBank
+
+# Dependencies
+import csv
+import os
+
+# Read csv file
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
+# The total number of months included in the dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in profits (date and amount) over the entire period
