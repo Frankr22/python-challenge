dbc48d0d189d49c72772d31b8cbb7afcb6cb16aa
Robert Franklin <robertlsfranklin@gmail.com>
2022-11-20 10:01:30 +0800

    worked on main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,26 +5,32 @@ import csv
 import os
 
 # Read csv file
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('..', 'PyBank\Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    # Read the header row and add to list
+    csv_budget = []
+    csv_budget = next(csvreader)
 
-    # Read the header row
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
+    # Read each row of data after the header and add to list
     for row in csvreader:
-        print(row)
+        csv_budget.append(row)
+
+#print(csv_budget[2:])
 
 # The total number of months included in the dataset
+print(f"There are {len(csv_budget)-2} months included in the dataset")
 
 # The net total amount of "Profit/Losses" over the entire period
+profit_count = 0
+loss_count = 0
+for month in csv_budget[2:]:
+    if month > 0: profit_count += 1
+    else:loss_count += 1
 
 # The changes in "Profit/Losses" over the entire period, and then the average of those changes
 
